e8b15bbb19935ee990314f91d22e1b9b81932fc4
MichalLecznar <misiuleczi13@gmail.com>
2020-11-05 19:54:42 +0100

    dodawanie nowego użytkownika

---
+++ src/main/java/pl/coderslab/users/UserAdd.java
@@ -0,0 +1,33 @@
+package pl.coderslab.users;
+
+import pl.coderslab.utils.User;
+import pl.coderslab.utils.UserDao;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/user/add")
+public class UserAdd extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        getServletContext().getRequestDispatcher("/users/add.jsp").forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        User user = new User();
+        user.setUserName(request.getParameter("userName"));
+        user.setEmail(request.getParameter("mail"));
+        user.setPassword(request.getParameter("password"));
+        UserDao userDao = new UserDao();
+        userDao.create(user);
+        response.sendRedirect("/user/list");
+    }
+}
+   
